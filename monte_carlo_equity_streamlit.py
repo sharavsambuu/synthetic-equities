2028bb6ac19bf362e296ecd1682a890502a52daa
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2023-07-20 06:46:23 +0800

    added quantstats lib for some metric extraction from equity curve.

--- monte_carlo_equity_streamlit.py
+++ monte_carlo_equity_streamlit.py
@@ -1,11 +1,12 @@
+import multiprocessing
 import streamlit         as st
 import pandas            as pd
 import numpy             as np
+import quantstats        as qs
 import matplotlib.pyplot as plt
-from   datetime             import timedelta
-from   backtesting          import Backtest, Strategy
-import multiprocessing
-from   multiprocessing      import Pool
+from   datetime          import timedelta
+from   backtesting       import Backtest, Strategy
+from   multiprocessing   import Pool
 
 
 def main():
